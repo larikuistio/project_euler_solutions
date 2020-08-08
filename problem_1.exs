7652385dafad1f77775b36feec5ee082351619d6
lari <lari@kuistio.me>
2020-08-08 12:21:13 +0300

    first commit

---
+++ problem_1.exs
@@ -0,0 +1,13 @@
+defmodule Problem1 do
+    def isMultiple(n) do
+        cond do
+            rem(n,3) == 0 -> n
+            rem(n,5) == 0 -> n
+            rem(n,5) != 0 and rem(n,3) != 0 -> 0
+        end
+    end
+end
+
+sum = 1..999 |> Enum.map(&Problem1.isMultiple(&1)) |> Enum.sum
+
+IO.inspect sum
